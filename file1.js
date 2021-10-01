ea703962206094f531ca0f293621370875594518
SriniTechHub <tenalisreenivas@gmail.com>
2021-10-01 12:23:09 +0530

    added more console.logs

--- file1.js
+++ file1.js
@@ -1,2 +1,7 @@
 console.log("hi from file1 ");
 console.log("don't commit this I made this changes now and I am going to delete this things here");
+console.log("don't commit this I made this changes now and I am going to delete this things here");
+console.log("don't commit this I made this changes now and I am going to delete this things here");
+console.log("don't commit this I made this changes now and I am going to delete this things here");
+console.log("don't commit this I made this changes now and I am going to delete this things here");
+console.log("don't commit this I made this changes now and I am going to delete this things here");
